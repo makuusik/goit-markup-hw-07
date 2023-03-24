8cd55cbed02da754b84cfcfd70edb6b934799136
makuusik <119566111+makuusik@users.noreply.github.com>
2023-03-24 03:45:23 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-  const mobileMenu = document.querySelector("[js-menu-container]");
-  const openMenuBtn = document.querySelector("[js-open-menu]");
-  const closeMenuBtn = document.querySelector("[js-close-menu]");
+  const mobileMenu = document.querySelector("[data-menu-container]");
+  const openMenuBtn = document.querySelector("[data-open-menu]");
+  const closeMenuBtn = document.querySelector("[data-close-menu]");
 
   const toggleMenu = () => {
     const isMenuOpen =
